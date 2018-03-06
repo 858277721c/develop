7e6dc12b08d31a3dc76005b449187ae94a749b09
zjun <565061763@qq.com>
2018-03-06 17:22:56 +0800

    1.0.8

--- lib/src/main/java/com/fanwe/lib/develop/business/FBusiness.java
+++ lib/src/main/java/com/fanwe/lib/develop/business/FBusiness.java
@@ -2,6 +2,12 @@ package com.fanwe.lib.develop.business;
 
 import com.fanwe.lib.develop.callback.FBSProgressCallback;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Proxy;
+import java.lang.reflect.Type;
+
 /**
  * Created by zhengjun on 2017/10/31.
  */
@@ -16,6 +22,36 @@ public abstract class FBusiness<T>
 
     public T getCallback()
     {
+        if (mCallback == null)
+        {
+            final ParameterizedType parameterizedType = (ParameterizedType) getClass().getGenericSuperclass();
+            final Type[] types = parameterizedType.getActualTypeArguments();
+            if (types != null && types.length == 1)
+            {
+                final Class<T> clazz = (Class<T>) types[0];
+                if (clazz.isInterface())
+                {
+                    mCallback = (T) Proxy.newProxyInstance(clazz.getClassLoader(), new Class<?>[]{clazz}, new InvocationHandler()
+                    {
+                        @Override
+                        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
+                        {
+                            final Class returnType = method.getReturnType();
+                            if (returnType.isPrimitive())
+                            {
+                                return 0;
+                            } else
+                            {
+                                return null;
+                            }
+                        }
+                    });
+                }
+            } else
+            {
+                throw new RuntimeException("generic type length must be 1");
+            }
+        }
         return mCallback;
     }
 
